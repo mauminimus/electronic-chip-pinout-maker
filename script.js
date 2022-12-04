ba57e5485b8e30eec67ca997419db55d08f86b71
mauminimus <119792826+mauminimus@users.noreply.github.com>
2022-12-04 14:59:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let s = ["", "", "", "", "", ""];
+
+function SetPinNumber() {
+
+    const sides = document.querySelectorAll('#leftboxtext, #leftbox, #leftboxnumbers, #rightboxnumbers, #rightbox, #rightboxtext');
+    let pinAmmount = document.getElementById("pinAmmount_number").value * 1;
+
+    /*for (let i = 0; i < sides.length; i++) {
+        sides[i].innerHTML = "";
+    }
+
+    for (let i = 0; i < pinAmmount/2; i++) {
+        sides[0].innerHTML += '<input type="text" class="" id="text' +(i+1)+ '">';
+        sides[1].innerHTML += '<div class="pinLeft"></div>';
+        sides[2].innerHTML += '<input type="button" value="' +(i+1)+ '" onclick="underline(' +(i+1)+ ');">';
+        sides[3].innerHTML += '<div class="pinRight"></div>';
+        sides[4].innerHTML += '<input type="text" class="" id="' +(pinAmmount-i)+ '">';
+        sides[5].innerHTML += '<input type="button" value="' +(pinAmmount-i)+ '" onclick="underline(' +(pinAmmount-i)+ ');">';
+    }*/
+
+    for (let i = 0; i < sides.length; i++) {
+        s[i] = "";
+    }
+
+    for (let i = 0; i < pinAmmount/2; i++) {
+        s[0] += '<input type="text" id="text' +(i+1)+ '">';
+        s[1] += '<div class="pinLeft"></div>';
+        s[2] += '<input type="button" value="' +(i+1)+ '" onclick="ul(' +(i+1)+ ');">';
+        s[3] += '<div class="pinRight"></div>';
+        s[4] += '<input type="text" id="text' +(pinAmmount-i)+ '">';
+        s[5] += '<input type="button" value="' +(pinAmmount-i)+ '" onclick="ul(' +(pinAmmount-i)+ ');">';
+    }
+
+    for (let i = 0; i < sides.length; i++) {
+        sides[i].innerHTML = s[i];
+    }
+}
+
+function ul(pinNumber) {
+    let text = document.querySelector('#text' +pinNumber);
+    if (text.classList.contains("underlined")) {
+        text.classList.remove("underlined");
+    }
+    else {
+        text.classList.add("underlined");
+    }
+}
